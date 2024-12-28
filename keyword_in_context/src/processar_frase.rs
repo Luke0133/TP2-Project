6bc22f42f3c5256d4d3ac4508ed63a9ee18d1e34
José Neto Souza <jos.souzalima@gmail.com>
2024-12-28 16:35:29 -0300

    Alteração na função processar_frase

--- keyword_in_context/src/processar_frase.rs
+++ keyword_in_context/src/processar_frase.rs
@@ -1,7 +1,7 @@
 use std::collections::HashSet;
 
 /// # Parâmetros
-/// - `frase`: Uma string contendo a frase a ser processada.
+/// - `frase`: Um vetor contendo as palavras a serem processadas.
 /// - `stop_words_set`: Um HashSet contendo as stop words a serem ignoradas (HashSet tem complexidade O(1) para busca).
 ///
 /// # Retorno
@@ -11,7 +11,10 @@ use std::collections::HashSet;
 /// ```
 /// use std::collections::HashSet;
 ///
-/// let frase = "O projeto de ISC é de mais para o primeiro semestre.";
+/// let frase = vec![
+///       "O".to_string(),"projeto".to_string(),"de".to_string(),"ISC".to_string(),"é".to_string(),"de".to_string(),
+///       "mais".to_string(),"para".to_string(),"o".to_string(),"primeiro".to_string(),"semestre".to_string(),];
+///
 /// let stop_words_set: HashSet<&str> = vec!["O", "de", "é", "mais", "para", "o"].into_iter().collect();
 /// let resultado = processar_frase(frase, &stop_words_set);
 /// ```
@@ -21,22 +24,20 @@ use std::collections::HashSet;
 ///     "primeiro | semestre O projeto de ISC é de mais para",
 ///     "semestre | O projeto de ISC é de mais para o primeiro"
 /// ];
-fn processar_frase(frase: &str, stop_words_set: &HashSet<&str>) -> Vec<String> {
+fn processar_frase(frase: Vec<String>, stop_words_set: &HashSet<&str>) -> Vec<String> {
     let mut frases_deslocadas = Vec::new();
 
-    let palavras: Vec<&str> = frase 
-        .split(|c: char| c.is_whitespace() || c.is_ascii_punctuation())
-        .filter(|s| !s.is_empty())
-        .collect();
+    for (indice, palavra) in frase.iter().enumerate() {
+        let palavra_sem_pontuacao: String = palavra.chars().filter(|c| c.is_alphanumeric()).collect();
 
-    for palavra in &palavras {
-
-        if !stop_words_set.contains(*palavra) {
-            let frase_deslocada = deslocamento_circular(palavra, &palavras);
+        if !stop_words_set.contains(palavra_sem_pontuacao.as_str()) {
+            let frase_deslocada = deslocamento_circular(indice, &frase);
             frases_deslocadas.push(frase_deslocada);
-            }
         }
     }
+
+    frases_deslocadas
+}
 
     frases_deslocadas
 }
